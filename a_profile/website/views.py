51862fbcfb6e92113942b2265497f2a0212d7f1c
iTheSailor <112807438+iTheSailor@users.noreply.github.com>
2023-08-27 21:45:12 -0400

    basic setup

---
+++ a_profile/website/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'website/home.html')
+
+def about(request):
+    return render(request, 'website/about.html')
+
+def contact(request):
+    return render(request, 'website/contact.html')
